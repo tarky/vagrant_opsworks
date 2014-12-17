957ba110ca08b67e0db356db8caeabf4df439ff3
tarky <taisuke.araki@gmail.com>
2014-12-17 19:53:14 +0900

    add pry-remote and make timeout longer #3

--- cookbooks/mimic_opsworks/recipes/link_local.rb
+++ cookbooks/mimic_opsworks/recipes/link_local.rb
@@ -15,6 +15,12 @@ include_recipe "mimic_opsworks::recreate_releases"
 
 template "/srv/www/#{node['app_name']}/current/config/database.yml" 
 
+file "/srv/www/#{node['app_name']}/shared/config/unicorn.conf" do
+  _file = Chef::Util::FileEdit.new(path)
+  _file.search_file_replace('timeout 60', 'timeout 10000')
+  _file.write_file
+end
+
 bash "/srv/www/#{node['app_name']}/shared/scripts/unicorn restart"
 
 template "/etc/profile.d/for_dev.sh"
