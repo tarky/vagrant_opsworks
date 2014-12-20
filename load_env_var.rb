811d28eef38fa7c503cacfdc69b6d1142a762470
tarky <taisuke.araki@gmail.com>
2014-12-21 00:59:07 +0900

    error happens when migration of vagrant provision, because there is no default.rb #3

--- load_env_var.rb
+++ load_env_var.rb
@@ -1,6 +1,9 @@
-File::open("./cookbooks/mimic_opsworks/attributes/default.rb") do |f|
-  f.each do |line|
-    match = line.scan(/default\[:(.*)\] = "(.*)"/)
-    ENV[match[0][0]] = match[0][1] if match[0] && !match[0][0].include?('][')
+filepath = "./cookbooks/mimic_opsworks/attributes/default.rb"
+if File.exists? filepath
+  File::open filepath do |f|
+    f.each do |line|
+      match = line.scan(/default\[:(.*)\] = "(.*)"/)
+      ENV[match[0][0]] = match[0][1] if match[0] && !match[0][0].include?('][')
+    end
   end
 end
