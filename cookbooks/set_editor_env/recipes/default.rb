60443280ff911424d75ba7a8774cabb9feeec751
tarky <taisuke.araki@gmail.com>
2015-02-26 15:45:52 +0000

    add hub and golang which hub depends on.

--- cookbooks/set_editor_env/recipes/default.rb
+++ cookbooks/set_editor_env/recipes/default.rb
@@ -12,5 +12,18 @@ git "/root/dotfiles" do
   repository "https://github.com/tarky/dotfiles.git"
   enable_submodules true
 end
-
 execute "sh /root/dotfiles/dotfilesLink.sh"
+
+#go and hub
+directory "/root/gocode"
+cookbook_file "/etc/profile.d/for_hub.sh"
+remote_file "/tmp/go.tar.gz" do
+  source "https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz"
+end
+execute "tar -C /usr/local -xzf /tmp/go.tar.gz"
+execute "go get github.com/github/hub" do
+  environment(
+    "GOPATH" => "/root/gocode",
+    "PATH" => "/usr/local/go/bin:#{ENV["PATH"]}"
+  )
+end
