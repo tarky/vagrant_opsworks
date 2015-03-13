8076203c2da916751db7760ce773433bbfd56479
tarky <taisuke.araki@gmail.com>
2015-03-13 05:02:48 +0000

    change attribute name to capital character to make them look like Environment variables. #142 #143

--- cookbooks/mimic_opsworks/recipes/link_local.rb
+++ cookbooks/mimic_opsworks/recipes/link_local.rb
@@ -1,21 +1,21 @@
-link "/srv/www/#{node['app_name']}/current" do
+link "/srv/www/#{node[:APP_NAME]}/current" do
   action :delete
 end
 
-link "/srv/www/#{node['app_name']}/current" do
+link "/srv/www/#{node[:APP_NAME]}/current" do
   to "/vagrant"
 end
 
 execute "copy .bundle" do
-  cwd "/srv/www/#{node['app_name']}"
+  cwd "/srv/www/#{node[:APP_NAME]}"
   command "cp -r releases/*/.bundle current"
 end
 
 include_recipe "mimic_opsworks::recreate_releases"
 
-template "/srv/www/#{node['app_name']}/current/config/database.yml"
+template "/srv/www/#{node[:APP_NAME]}/current/config/database.yml"
 
-file "/srv/www/#{node['app_name']}/shared/config/unicorn.conf" do
+file "/srv/www/#{node[:APP_NAME]}/shared/config/unicorn.conf" do
   _file = Chef::Util::FileEdit.new(path)
   _file.search_file_replace('timeout 60', 'timeout 10000')
   _file.write_file
